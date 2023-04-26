a4b8110ba72e83a4f42a1f090d46fda562ddd0f7
Susan Shrestha <susanstha29gmail.com>
2023-04-26 14:02:05 +0545

    Hello world

---
+++ conditional.dart
@@ -0,0 +1,8 @@
+void main() {
+  bool isAdmin = false;
+  if (isAdmin) {
+    print("verified admin");
+  } else {
+    print("not verified");
+  }
+}
---
+++ datatypes.dart
@@ -0,0 +1,4 @@
+void main() {
+  String name = " Susan ";
+  print(name.trim().length);
+}
